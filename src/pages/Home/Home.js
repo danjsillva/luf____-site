458f928888dc88d234666c95eb0d07d822ede304
Daniel Silva <danielsilva@Daniels-MacBook-Air.local>
2019-04-18 12:39:52 -0300

    Adicionado section missao, visao, valores

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -77,7 +77,9 @@ export default function Home(props) {
             style={{ marginTop: "50vh" }}
           >
             <h2>
-              UMA BUSCA PERMANENTE PELA <strong className="text-warnin">EXCELÊNCIA E QUALIDADE</strong> DOS PRODUTOS COMERCIALIZADOS
+              UMA BUSCA PERMANENTE PELA{" "}
+              <strong className="text-warnin">EXCELÊNCIA E QUALIDADE</strong>{" "}
+              DOS PRODUTOS COMERCIALIZADOS
             </h2>
           </div>
         </div>
@@ -86,11 +88,45 @@ export default function Home(props) {
       <PageDownLink />
 
       <section className="bg-warning">
-        <div className="container py-5">asd</div>
+        <div className="container py-5">
+          <div className="row mt-4 mb-5">
+            <div class="col-4 mvv-card">
+              <div className="mvv-number">1</div>
+              <div className="mvv-text bg-warning">
+                <h6>
+                  <strong>MISSÃO</strong>
+                </h6>{" "}
+                A Missão da Lufarma define-se na comercialização de produtos que
+                promovam saúde e bem estar dos indivíduos.
+              </div>
+            </div>
+            <div class="col-4 mvv-card">
+              <div className="mvv-number">2</div>
+              <div className="mvv-text bg-warning">
+                <h6>
+                  <strong>VISÃO</strong>
+                </h6>{" "}
+                Buscar permanentemente um grau de excelência no conhecimento e
+                na qualidade dos produtos comercializados, compartilhando
+                experiências para o beneficio dos seus clientes.
+              </div>
+            </div>
+            <div class="col-4 mvv-card">
+              <div className="mvv-number">3</div>
+              <div className="mvv-text bg-warning">
+                <h6>
+                  <strong>VALORES</strong>
+                </h6>{" "}
+                Cliente como razão da existência da organização. Relacionamento
+                ético e transparente com clientes e investidores.
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
 
       <section id="section1" className="">
-        <div className="container py-5">asd</div>
+        <div className="container py-5" />
       </section>
 
       <br />
